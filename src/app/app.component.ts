0d3d295f1894bce007f70040359d2a8166dd6915
franaragm <franaragonmesa@gmail.com>
2018-05-30 12:12:37 +0200

    demostración de uso de pipes y creación de pipes personlaizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Fernando";
+  nombre2 = "fernando alberto heRREra jImeNez";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "logan",
+    clave: "wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout( ()=>resolve('Llego la data'),3500 );
+  } )
+
+  fecha = new Date();
+
+  video = "jnp_ny4SOQE";
+
+  activar:boolean = true;
+
+
 }
